964e552d43dd22fa3bbd16bde660a2bff084d6c2
João Camillis <163936465+joaocamillis@users.noreply.github.com>
2025-02-13 01:39:23 -0300

    Add files via upload

---
+++ 2°DS-AMS/Programação Web II/1° Bimestre/Exemplos de PHP Básico/Exercicio1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 02</title>
+</head>
+
+<body>
+    <?php
+    echo"oii mundo que usa o PHP, este é meu programa"."<br>";
+    echo"Quero aprender bastante sobre essa linguagem";
+    echo"durante as aulas!!!";
+    ?>                  
+</body>
+
+</html>
