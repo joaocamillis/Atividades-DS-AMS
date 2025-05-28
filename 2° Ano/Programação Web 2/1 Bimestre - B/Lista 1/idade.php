6eca088effd68c36644e04f6875c1c5a9e37539d
joaocamillis <joaocomercial2233@gmail.com>
2025-05-28 01:53:06 -0300

    .

---
+++ 2° Ano/Programação Web 2/1 Bimestre - B/Lista 1/idade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>idade</title>
+</head>
+<body>
+    <?php
+    $idade="15";
+
+    echo"eu tenho ". $idade ." anos";
+    
+    ?>
+</body>
+</html>
---
+++ 2° Ano/Programação Web 2/1 Bimestre - B/Lista 1/nome_e_sobrenome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nome e sobrenome</title>
+</head>
+<body>
+    <?php
+    
+    $nome="joao pedro ";
+    $sobrenome="Souza";
+
+    echo "meu nome é ". $nome . " "  . $sobrenome ." " ;
+
+
+    
+    ?>
+</body>
+</html> 
